6ba70ca1618c7b7d78c87c4f8d09ebe0f4e78b5a
imniteen <109958419+imniteen@users.noreply.github.com>
2025-04-12 16:56:09 +0530

    Add files via upload

---
+++ HelperFunctions.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ProactiveBot.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProactiveBot
+{
+    public class HelperFunctions
+    {
+        public async static Task InvokeWorkflow(
+            string userQuery,
+            string myproductPluginKey = null,
+            string myproductPluginName = null,
+            bool isAction = false,
+            string workflowName = null,
+            string token = null)
+        {
+            string url = $"https://aimlivt-business-api.myproduct.com/ExploreAIService/v3/myproductcopilot/query";
+            var jsonData = new
+            {
+                data = new
+                {
+                    queryText = userQuery,
+                    isUserQuery = false,
+                    requestId = "",
+                    stream = true
+                }
+            };
+
+            JObject jsonObject = JObject.FromObject(jsonData);
+            if (jsonObject["data"] != null)
+            {
+                jsonObject["data"]["userSelections"] = new JArray();
+                if (!string.IsNullOrEmpty(myproductPluginKey))
+                {
+                    jsonObject["data"]["userSelections"] = JArray.FromObject(new List<string>()
+                    {
+                        myproductPluginKey
+                    });
+                }
+
+                List<NameValueJObject> additionalContext = new List<NameValueJObject>();
+                additionalContext.Add(new NameValueJObject()
+                {
+                    name = "isCorpusQuery",
+                    value = true
+                });
+                additionalContext.Add(new NameValueJObject()
+                {
+                    name = "envelopResponseType",
+                    value = "seq"
+                });
+
+                if (!string.IsNullOrEmpty(workflowName))
+                {
+                    additionalContext.Add(new NameValueJObject()
+                    {
+                        name = "workflowName",
+                        value = workflowName
+                    });
+                }
+
+                string actionJsonString = @"
+                    {
+                        'name': 'Action',
+                        'value': {
+                            'plugin_id': null,
+                            'label': null,
+                            'plugin_name': 'get_data',
+                            'action_name': null,
+                            'parameters': [
+                                {
+                                    'name': 'query_text',
+                                    'description': null,
+                                    'value': null,
+                                    'type': 'string'
+                                },
+                                {
+                                    'name': 'additional_context_filters',
+                                    'description': null,
+                                    'value': null,
+                                    'type': 'object'
+                                },
+                                {
+                                    'name': 'rephrased_query',
+                                    'description': null,
+                                    'value': null,
+                                    'type': 'string'
+                                }
+                            ]
+                        }
+                    }";
+                JObject actionJsonObj = JObject.Parse(actionJsonString);
+                actionJsonObj["value"]["plugin_name"] = myproductPluginName;
+
+                NameValueJObject nameValueObject = new NameValueJObject
+                {
+                    name = actionJsonObj["name"].ToString(),
+                    value = actionJsonObj["value"]
+                };
+                additionalContext.Add(nameValueObject);
+
+                jsonObject["data"]["additionalContext"] = JArray.FromObject(additionalContext);
+            }
+
+            string jsonString = JsonConvert.SerializeObject(jsonObject);
+
+            StringContent content = new(jsonString, Encoding.UTF8, "application/json");
+
+            using HttpClient httpClient = new();
+            httpClient.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            httpClient.DefaultRequestHeaders.Add("IcmAuthToken", $"{token}");
+            HttpRequestMessage request = new(HttpMethod.Post, url)
+            {
+                Content = content
+            };
+
+            
+            using (HttpResponseMessage response = await httpClient.SendAsync(
+                request,
+                HttpCompletionOption.ResponseHeadersRead,
+                default(CancellationToken)))
+            {
+                using (Stream stream = await response.Content.ReadAsStreamAsync(default(CancellationToken)))
+                {
+                    using (StreamReader reader = new(stream))
+                    {
+                    }
+                }
+            }
+        }
+
+        public static string GetWorkflowName(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            // Find the position of the last '/' and the ':' delimiter
+            int startIndex = input.IndexOf('/') + 1; // Start after the '/'
+            int endIndex = input.IndexOf(':', startIndex); // Find ':' after the '/'
+
+            if (startIndex > 0)
+            {
+                if (endIndex > startIndex)
+                {
+                    // Extract substring between '/' and ':'
+                    return input.Substring(startIndex, endIndex - startIndex).Trim();
+                }
+                else
+                {
+                    // Extract from '/' to the end if ':' is not found
+                    return input.Substring(startIndex).Trim();
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.BotBuilderSamples
+{
+    using System;
+    using System.Collections.Concurrent;
+    using Azure.Identity;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Builder.Integration.AspNet.Core;
+    using Microsoft.Bot.Connector.Authentication;
+    using Microsoft.Bot.Schema;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using ProactiveBot;
+    using ProactiveBot.Models;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            //var version = builder
+            //    .Configuration
+            //    .GetRequiredSection("Version")?.Value
+            //    ?? throw new NullReferenceException("The version value cannot be found.");
+
+            //if (builder.Environment.IsDevelopment())
+            //    InitializeDevEnvironment(builder, version);
+            //else
+            //    InitializeProdEnvironment(builder, version);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddHttpClient().AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.MaxDepth = HttpHelper.BotMessageSerializerSettings.MaxDepth;
+            });
+
+            // Create the Bot Framework Authentication to be used with the Bot Adapter.
+            builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+            // Create the Bot Adapter with error handling enabled.
+            builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create a global hashset for our ConversationReferences
+            builder.Services.AddSingleton<ConcurrentDictionary<string, ConversationReference>>();
+
+            // Create a global hashset for in-memory data store
+            builder.Services.AddSingleton<ConcurrentDictionary<string, InMemoryStore>>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            builder.Services.AddTransient<IBot, ProactiveNotificationBot>();
+
+            var app = builder.Build();
+
+            if (builder.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            app.Run();
+        }
+
+        private static void InitializeProdEnvironment(WebApplicationBuilder builder, string version)
+        {
+            var appConfigurationEndpoint = builder.Configuration.GetRequiredSection
+                ("AppConfiguration:Endpoint")?.Value
+                ?? throw new Exception();
+
+            var managedIdentityClientId = builder.Configuration.GetRequiredSection
+                ("UserAssignedManangedIdentityClientId")?.Value
+                ?? throw new Exception();
+
+            ManagedIdentityCredential userAssignedManagedCredentials = new(managedIdentityClientId);
+            builder.Configuration.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(new Uri(appConfigurationEndpoint), userAssignedManagedCredentials)
+                    .ConfigureKeyVault(kv => kv.SetCredential(userAssignedManagedCredentials))
+                    .Select(KeyFilter.Any, version);
+            });
+        }
+
+        private static void InitializeDevEnvironment(WebApplicationBuilder builder, string version)
+        {
+            var appConfigurationConnectionString = builder
+                .Configuration
+                .GetConnectionString("AppConfig")
+                ?? throw new Exception();
+
+            AzureCliCredential azureCliCredential = new();
+
+            builder.Configuration.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(appConfigurationConnectionString)
+                    .ConfigureKeyVault(kv => kv.SetCredential(azureCliCredential))
+                    .Select(KeyFilter.Any, version);
+            });
+        }
+    }
+}
